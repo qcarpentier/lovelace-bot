dbf0ab4c89efb85417091c43ed8a71f1f01d3a88
Quentin Carpentier <quentin.carpentier@outlook.be>
2021-01-12 12:37:59 +0100

    🎨 Comments and some refactoring

--- index.js
+++ index.js
@@ -1,36 +1,50 @@
-const Discord = require('discord.js');
-const fs = require('fs');
-require('dotenv').config();
+const Discord = require("discord.js");
+const fs = require("fs");
+require("dotenv").config();
 const { BOT_TOKEN, PREFIX } = process.env;
 
 const bot = new Discord.Client();
 bot.commands = new Discord.Collection();
 
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+// Dynamically reading command files
+const commandFiles = fs
+  .readdirSync("./commands")
+  .filter((file) => file.endsWith(".js"));
 
 for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	bot.commands.set(command.name, command);
+  const command = require(`./commands/${file}`);
+  bot.commands.set(command.name, command);
 }
 
-bot.on('ready', () => {
-    console.log("Bot started.");
+// Runs whenever the bot is up & running
+bot.on("ready", () => {
+  console.log("Bot started.");
 });
 
 // Runs whenever a new user is added to the server
-bot.on("guildMemberAdd", member => {
+bot.on("guildMemberAdd", (member) => {
   console.log("New member!");
 });
 
-bot.on('message', message => {
+// Runs whenever a new message has been sent
+bot.on("message", (message) => {
   if (!message.content.startsWith(PREFIX) || message.author.bot) return;
 
-  const args = message.content.slice(PREFIX.length).trim().split(' ');
+  const args = message.content.slice(PREFIX.length).trim().split(" ");
   const command = args.shift().toLowerCase();
 
-  if (command === 'ping') {
-		bot.commands.get('ping').execute(message, args);
-	} 
+  if (!bot.commands.has(command))
+    return message.channel.send(
+      "Malheureusement, je ne connais pas encore cette commande. Vous pouvez proposer votre idée dans le channel `#suggestions`!"
+    );
+
+  // Dynamically executing commands
+  try {
+    bot.commands.get(command).execute(message, args);
+  } catch (error) {
+    console.error(error);
+    message.reply("Il y a eu une erreur quand j'ai exécuté la commande!");
+  }
 });
 
 bot.login(BOT_TOKEN);
